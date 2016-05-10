af1e9b6fed55960322f8727b17628acc13282928
Ratan <ratan@Ratans-MacBook-Pro.local>
2016-05-10 22:20:25 +0530

    major upheavel

---
+++ public/js/spectatorchess.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+     
+    var board = ChessBoard('chessboard', {
+        draggable: true,
+        dropOffBoard: 'trash',
+        sparePieces: true
+    });
+
+});
